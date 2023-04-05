d4a3bd0912158406133aef4a6281e37da8c7ce14
Maksym Triteinikov <i.max.trit@gmail.com>
2023-04-05 22:04:47 +0300

    added options
    to basicLightBox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,13 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const gallery = document.querySelector(".gallery");
+const gallery = document.querySelector('.gallery');
 
 // Створення галереї
-function createGallery (arr, listEl) {
-    const markup = arr.map(({ preview, original, description }) => `<li class="gallery__item">
+function createGallery(arr, listEl) {
+  const markup = arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
@@ -14,32 +16,45 @@ function createGallery (arr, listEl) {
       alt="${description}"
     />
   </a>
-</li>`).join("");
-    listEl.insertAdjacentHTML("beforeend", markup);
+</li>`
+    )
+    .join('');
+  listEl.insertAdjacentHTML('beforeend', markup);
 }
 createGallery(galleryItems, gallery);
 
 // Лайтбокс
-gallery.addEventListener("click", showLightBox);
+gallery.addEventListener('click', showLightBox);
 
 function showLightBox(event) {
-    event.preventDefault()    
-    if (event.target.nodeName !== "IMG") {
-        return
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  };
+
+  const escapeClosing = event => {
+    if (event.key === 'Escape') {
+      lightBox.close();
     }
-
-    const lightBox = basicLightbox.create(`
+    return;
+  };
+  
+  const lightBox = basicLightbox.create(
+    `
     <img src="${event.target.dataset.source}" alt="${event.target.alt}" width="800" height="600">
-`);
-    
-    lightBox.show()
-
+`,
     // Закриття по Escape
-    gallery.addEventListener("keydown", (event) => {
-        if (event.key === "Escape") {
-           lightBox.close()
-        }
+    {
+      onShow: () => {
+        gallery.addEventListener('keydown', escapeClosing);
         return;
- })
-}
+      },
 
+      onClose: () => {
+        gallery.removeEventListener('keydown', escapeClosing);
+        return;
+      }
+    }
+  );
+  lightBox.show();
+}
